8e4d307104ea42e3aa88c03ed3878429bd171654
EliasMenn <eliasmennella10@gmail.com>
2025-05-20 13:59:26 -0300

    Se agrega la creacion de BD, Esquemas e inicio de tablas
    Agregue la creacion de la BD, de los esquemas y generacion de las tablas pertenencientes al esquema Person (Nuevo Commit ya que falto parte de la generacion de tablas)

--- Com5600_G12/00_CreacionTablasYEstructuras.sql
+++ Com5600_G12/00_CreacionTablasYEstructuras.sql
@@ -24,24 +24,29 @@ GO
 -- Genere esquemas para organizar de forma lógica los componentes del sistema y aplique esto en la creación de objetos. NO use el esquema “dbo”. --
 
 IF SCHEMA_ID('Person') IS NULL
+BEGIN
 	EXEC('CREATE SCHEMA Person'); --El Schema User se vinculara con las tablas de Persona, Rol, Usuario, Tutor, Socio.
-GO
+END
 
 IF SCHEMA_ID('Payment') IS NULL
+BEGIN
 	EXEC('CREATE SCHEMA Payment'); --El Schema User se vinculara con las tablas de Factura, Morosidad, Detalle_Factura, Pago, Medio_Pago, Tipo_Medio, Cuenta.
-GO
+END
 
 IF SCHEMA_ID('Activity') IS NULL
+BEGIN
 	EXEC('CREATE SCHEMA Activity'); --El Schema Activity se vinculara con las tablas de Actividad, Horario_Actividad, Inscripto_Actividad, Actividad_Extra, Inscripto_Act_Extra
-GO
+END
 
 IF SCHEMA_ID('Groups') IS NULL
+BEGIN
 	EXEC('CREATE SCHEMA Groups'); --El Schema Groups se vinculara con las tablas de Grupo_Familiar, Miembro_Familia, Categoria.
-GO
+END
 
 IF SCHEMA_ID('Day') IS NULL
+BEGIN
 	EXEC('CREATE SCHEMA Day'); --El Schema Day se utilizara para la tabla de Jornada
-GO
+END
 
 ------------------ CREACIÓN DE TABLAS -------------------
 -- Cree las entidades y relaciones. Incluya restricciones y claves --
@@ -68,5 +73,54 @@ BEGIN
 		Parentesco VARCHAR (20),
 		CONSTRAINT FK_Tutor_Persona
 		FOREIGN KEY (Id_Persona) REFERENCES Person.Persona(Id_Persona)
+	);
+END
+
+IF OBJECT_ID('Person.Socio', 'U') IS NULL
+BEGIN
+	CREATE TABLE Person.Socio
+	(
+		Id_Socio INT IDENTITY(1,1) PRIMARY KEY,
+		Id_Persona INT NOT NULL,
+		Id_Categoria INT NOT NULL,
+		Id_Tutor INT,
+		Telefono_Emergencia VARCHAR(15),
+		Obra_Social VARCHAR(30),
+		Nro_Obra_Social VARCHAR(20),
+		CONSTRAINT FK_Socio_Persona
+		FOREIGN KEY (Id_Persona) REFERENCES Person.Persona(Id_Persona),
+		/*CONSTRAINT FK_Socio_Categoria
+		FOREIGN KEY (Id_Categoria) REFERENCES Groups.Categoria(Id_Categoria),*/
+		CONSTRAINT FK_Socio_Tutor
+		FOREIGN KEY (Id_Tutor) REFERENCES Person.Tutor(Id_Tutor)
+
+	);
+END
+
+IF OBJECT_ID('Person.Rol','U') IS NULL
+BEGIN
+	CREATE TABLE Person.Rol
+	(	
+		Id_Rol Integer PRIMARY KEY,
+		Nombre_Rol VARCHAR(25),
+		Desc_Rol VARCHAR(50)
+	)
+END
+
+IF OBJECT_ID('Person.Usuario', 'U') IS NULL
+BEGIN
+	CREATE TABLE Person.Usuario
+	(
+		Id_Usuario INT IDENTITY(1,1) PRIMARY KEY,
+		Id_Rol INT NOT NULL,
+		Id_Persona INT NOT NULL,
+		Nombre_Usuario VARCHAR(30),
+		Contraseña VARCHAR(25),
+		Vigencia_Contraseña DATE,
+		CONSTRAINT FK_Usuario_Rol
+		FOREIGN KEY (Id_Rol) REFERENCES Person.Rol(Id_Rol),
+		CONSTRAINT FK_Usuario_Persona
+		FOREIGN KEY (Id_Persona) REFERENCES Person.Persona(Id_Persona),
+
 	);
 END
